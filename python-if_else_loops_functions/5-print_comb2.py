6337552c37cfae8c6034da91452588bf6c6c1123
Koralys Figueroa <koralysfigueroa12@gmail.com>
2026-03-16 11:18:13 -0400

    hola

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-"""Print numbers from 00 to 99 separated by ", ".
 
-Constraints:
-- no more than 2 print functions with string format
-- only one loop
-- do not store numbers or strings in a variable
-- do not import any module
-"""
-
-for i in range(100):
-    if i != 99:
-        print("{:02d}, ".format(i), end="")
+for number in range(100):
+    if number < 99:
+        print("{:02d}".format(number), end=', ')
     else:
-        print("{:02d}".format(i))
+        print(number)
