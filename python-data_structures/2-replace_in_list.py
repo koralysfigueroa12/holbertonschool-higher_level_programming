6d72439f040ce6992a4510c78a4226b9827dc717
Koralys Figueroa <koralysfigueroa12@gmail.com>
2026-03-16 12:17:38 -0400

    hola

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    """Replace element at idx of my_list with element if idx valid.
+    """Replace element at idx of my_list with element.
 
-    If idx is negative or out of range, do nothing and return the original list.
+    If idx is negative or out of range, do nothing and return
+    the original list.
     """
     if idx < 0 or idx >= len(my_list):
         return my_list
