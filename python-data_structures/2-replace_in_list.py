6902a637145f3f3da3ca8a989ba282d52f72f967
Koralys Figueroa <koralysfigueroa12@gmail.com>
2026-03-16 12:16:39 -0400

    hola

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Replace element at idx of my_list with element if idx valid.
+
+    If idx is negative or out of range, do nothing and return the original list.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
