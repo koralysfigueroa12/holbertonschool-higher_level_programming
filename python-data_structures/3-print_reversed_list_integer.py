093cb17c1fabe3b3884a304c0b4a99a8ea8ae4ef
Koralys Figueroa <koralysfigueroa12@gmail.com>
2026-03-16 12:19:26 -0400

    hola

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list in reverse order, one per line.
+
+    Uses str.format() and does not cast integers to strings.
+    """
+    for i in my_list[::-1]:
+        print("{:d}".format(i))
